7f2c1e1fe4f02c144ec868d22bcf244f4201ae6d
bhuchit <poochit1996@hotmail.com>
2022-06-13 14:41:08 +0700

    DAY 14-15: 14: Streamlit Components 15: st.latex

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,21 @@
 import streamlit as st
-from datetime import time, datetime
 import pandas as pd
-import numpy as np
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 
-st.header('st.selectbox')
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
-st.write('Your favorite color is ', option)
 
-st.header('st.multiselect')
-options = st.multiselect(
-     'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
-st.write('You selected:', options)
+st.header('st.latex')
 
-st.header('st.checkbox')
-st.write ('What would you like to order?')
-icecream = st.checkbox('Ice cream')
-coffee = st.checkbox('Coffee')
-cola = st.checkbox('Cola')
-if icecream:
-     st.write("Great! Here's some more 🍦")
-if coffee: 
-     st.write("Okay, here's some coffee ☕")
-if cola:
-     st.write("Here you go 🥤")
+st.latex(r'''
+     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+     \sum_{k=0}^{n-1} ar^k =
+     a \left(\frac{1-r^{n}}{1-r}\right)
+     ''')
+
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
+
